ce92702dec30d1838db5936b8fec7a147a85afb5
Lazar Stamenkovic <lsblue1995101@gmail.com>
2023-10-04 04:29:21 -0500

    fix product board note creation flow

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -15,7 +15,7 @@ import * as Intercom from "./intercom";
 import * as Firebase from "./firebase";
 import * as ProductBoard from "./product_board";
 
-import { HubspotTicketData } from "./types";
+import { HubspotTicketData, IntercomTicketType } from "./types";
 
 require('dotenv').config()
 
@@ -53,50 +53,75 @@ export const hubspotSubmit = onRequest(async (request, response) => { // webhook
 export const intercomTicketUpdated = onRequest(async (request, response) => { // webhook on intercom ticket updated
   logger.info({ "intercomTicketUpdated": request.body})
   try {
-    if (request.body.topic !== 'ticket.note.created') {
-      logger.info({ "success": false, msg: "not note created topic"})
-      response.status(200).send("not note created topic")
-    }
-    const intercomTicket = request.body.data?.item?.ticket
-    if (!intercomTicket) {
-      logger.info({ "success": false, msg: "can't get ticket data from request body"})
-      throw new Error("can't get ticket data from request body")
-    }
-    logger.info({ "intercomTicket": intercomTicket, msg: "success to get intercom ticket"})
-    // if (!['resolved', 'closed'].includes(intercomTicket.ticket_state)) {
-    //   logger.info({ "success": false, msg: "not resolved ticket"})
-    //   response.status(200).send("not resolved ticket")
-    //   return
-    // }
-    // if (intercomTicket.ticket_type.id !== IntercomTicketType.FeatureRequest) {
-    //   logger.info({ "success": false, msg: "not feature request ticket"})
-    //   response.status(200).send("not feature request ticket")
-    //   return
-    // }
-    const firebaseTicket = await Firebase.getTicketByIntercomTicketId(intercomTicket.id)
-    if (!firebaseTicket) {
-      logger.info({ "success": false, msg: "can't find firebase ticket"})
-      throw new Error("can't find firebase ticket")
-    }
-    const ticket_part = request.body.data?.item?.ticket_part
-    if (!ticket_part) {
-      logger.info({ "success": false, msg: "can't get ticket part from request body"})
-      throw new Error("can't get ticket part from request body")
-    }
-    const body: string = ticket_part.body
-    if (body && body.toLocaleLowerCase().includes('released') && body.includes('https://fitow.atlassian.net/browse')) { //jira ticket marked as released
+    if (request.body.topic === 'ticket.note.created') {
+      const intercomTicket = request.body.data?.item?.ticket
+      if (!intercomTicket) {
+        logger.info({ "success": false, msg: "can't get ticket data from request body"})
+        throw new Error("can't get ticket data from request body")
+      }
+      logger.info({ "intercomTicket": intercomTicket, msg: "success to get intercom ticket"})
+      // if (!['resolved', 'closed'].includes(intercomTicket.ticket_state)) {
+      //   logger.info({ "success": false, msg: "not resolved ticket"})
+      //   response.status(200).send("not resolved ticket")
+      //   return
+      // }
+      // if (intercomTicket.ticket_type.id !== IntercomTicketType.FeatureRequest) {
+      //   logger.info({ "success": false, msg: "not feature request ticket"})
+      //   response.status(200).send("not feature request ticket")
+      //   return
+      // }
+      const firebaseTicket = await Firebase.getTicketByIntercomTicketId(intercomTicket.id)
+      if (!firebaseTicket) {
+        logger.info({ "success": false, msg: "can't find firebase ticket"})
+        throw new Error("can't find firebase ticket")
+      }
+      const ticket_part = request.body.data?.item?.ticket_part
+      if (!ticket_part) {
+        logger.info({ "success": false, msg: "can't get ticket part from request body"})
+        throw new Error("can't get ticket part from request body")
+      }
+      const body: string = ticket_part.body
+      if (body && body.toLocaleLowerCase().includes('released') && body.includes('https://fitow.atlassian.net/browse')) { //jira ticket marked as released
+        const ticket =  await Hubspot.getTicketById(firebaseTicket.hubspot_ticket_id)
+        const result = await Hubspot.closeTicketById(firebaseTicket.hubspot_ticket_id)
+        if (!firebaseTicket.product_board_id) {
+          const prodRes = await ProductBoard.createNote(ticket as HubspotTicketData)
+          await Firebase.updateTicket(firebaseTicket.id, {product_board_id: prodRes.data?.id })  
+        }
+        response.status(200).send(result)
+      } else {
+        logger.info({ "success": false, msg: "not released note"})
+        throw new Error("not released note")
+      }  
+    } else if (request.body.topic === 'ticket.state.updated') {
+      const intercomTicket = request.body.data?.item
+      if (!['resolved', 'closed'].includes(intercomTicket.ticket_state)) {
+        logger.info({ "success": false, msg: "not resolved ticket"})
+        response.status(200).send("not resolved ticket")
+        return
+      }
+      if (intercomTicket.ticket_type.id !== IntercomTicketType.FeatureRequest) {
+        logger.info({ "success": false, msg: "not feature request ticket"})
+        response.status(200).send("not feature request ticket")
+        return
+      }
+      const firebaseTicket = await Firebase.getTicketByIntercomTicketId(intercomTicket.id)
+      if (!firebaseTicket) {
+        logger.info({ "success": false, msg: "can't find firebase ticket"})
+        throw new Error("can't find firebase ticket")
+      }
       const ticket =  await Hubspot.getTicketById(firebaseTicket.hubspot_ticket_id)
-      const result = await Hubspot.closeTicketById(firebaseTicket.hubspot_ticket_id)
       if (!firebaseTicket.product_board_id) {
         const prodRes = await ProductBoard.createNote(ticket as HubspotTicketData)
         await Firebase.updateTicket(firebaseTicket.id, {product_board_id: prodRes.data?.id })  
+        logger.info({ "success": true, msg: "update product board note"})
       }
-      response.status(200).send(result)
-    } else {
-      logger.info({ "success": false, msg: "not released note"})
-      throw new Error("not released note")
+      response.status(200).send("success to create product board note on ticket close")
+      return
+    } else  {
+      logger.info({ "success": false, msg: "not note created or state updated topic"})
+      response.status(200).send("not note created or state updated topic")
     }
-
   } catch(e: any) {
     logger.info({ "success": false, msg: e})
     response.status(500).send(e)
